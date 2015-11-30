7534b4301a9e701f2e40faef8a99c6f02d4026ba
keziaht <kezithomas@gmail.com>
2015-11-30 18:36:05 -0500

    Create os.cpp
    Basics

---
+++ os.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void startup(){
+
+  }
+
+  void Crint(int &a, int p[]){
+
+  }
+
+  void Dskint(int &a, int p[]){
+
+  }
+
+  void Tro(int &a, int p[]){
+
+  }
+
+  void Svc(int &a, int p[]){
+
+  }
+
+  void siodisk(int jobnum);
+  void siodrum(int jobnum, int jobsize, int coreaddress, int direction);
+//  void ontrace();
+  void offtrace();
+
+
+
